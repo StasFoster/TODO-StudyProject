2ad74e9ee996e8adf3191c5c68abb5348e4eb578
StasFoster <stanislav06052005@gmail.com>
2026-04-10 09:53:02 +0400

    Написали шаблоны

--- TODO/Authtification/urls.py
+++ TODO/Authtification/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('',views.enter, name="enter"),
+    path('register/<string:mode>/', auth)
 ]
--- TODO/Authtification/views.py
+++ TODO/Authtification/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def enter(request):
+    if request.user.is_authenticated:
+        return redirect("main")
+    else:
+        return redirect("register", mode='registr')
+    
+

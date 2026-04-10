0068809e0eac17fdfff54e595a6bd7c54e8ca3fe
StasFoster <stanislav06052005@gmail.com>
2026-04-10 13:19:04 +0400

    Прописали значительную часть конотроллера и сделали систему регистрации

--- TODO/Authtification/urls.py
+++ TODO/Authtification/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.enter, name="enter"),
-    path('register/<string:mode>/', auth)
+    path('authtificate/<str:mode>/', views.authtificate, name="auth"),
+    path('reg/', views.register, name="reg"),
+    path('login/', views.user_login, name="login"),
+    path('logout/', views.exit, name="logout"),
 ]
